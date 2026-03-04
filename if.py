c208aef0f99cfd41c1776f1540a018d2841f8b1d
segozgo <160878539+segozgo@users.noreply.github.com>
2026-03-04 22:16:58 +0900

    Update if.py

--- if.py
+++ if.py
@@ -1,15 +1,13 @@
 from random import *
 
-counter = 1
 approved = 0
 
-while counter <= 50:
-    ETA = randint(5,50)
+for ETA in range(1, 51):
+    ETA = randint(5, 50)
     if ETA <= 15:
         print(f'[o]'+str(ETA))
         approved += 1
     else:
         print(f'[]'+str(ETA))
-    counter += 1
 
 print(f'approved is',{approved})
